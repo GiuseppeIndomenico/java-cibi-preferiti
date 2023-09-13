0c0e0914b52bfda3b72041c98529f8a458feb50c
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-09-13 17:27:24 +0200

    array con cibi preferiti

---
+++ src/org/lesson/java/array/CibiPrefeiriti.java
@@ -0,0 +1,21 @@
+package org.lesson.java.array;
+
+import java.util.Arrays;
+
+public class CibiPrefeiriti {
+	public static void main(String[] args) {
+		
+		String[] food = { "lasagna", "anelli di cipolla", "Patatine fritte", "costoletta", "birra", "torta", "pizza con l'ananas"};
+		
+	System.out.println( "I miei cibi preferiti sono: " + food.length);
+	System.out.println("--------------------------------------------");
+	System.out.println("il mio preferito e' la " + food[0]);
+	System.out.println("--------------------------------------------");
+	System.out.println("quello che gradisco di meno e' la " + food[6]);
+	System.out.println("--------------------------------------------");
+	System.out.println("il cibo di mezzo  e' la " + food[3]);
+	
+	
+	
+	}
+}
